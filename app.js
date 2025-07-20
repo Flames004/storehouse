2ed7a109801a3bf7317c74da24f0e7cdad6a7d9d
Deepak Shukla <flamesofwisdom004@gmail.com>
2025-07-21 00:03:37 +0530

    add form route

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 
 const userRouter = require('./routes/user.routes');
 
+app.use(express.json());
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/user', userRouter);
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,4 +5,10 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
+router.post('/register', (req, res) => {
+    // Here you would handle the registration logic, e.g., saving user data to a database
+    console.log(req.body);
+    res.send('User registered successfully');
+});
+
 module.exports = router;
